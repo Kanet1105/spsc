51996cbc2955d0e9602ac74a86fdb51f7a80f4cb
kanet1105 <zombiedelah@gmail.com>
2025-01-31 17:30:32 +0900

    Update benchmarks

--- benchmarks/ring_buffer.rs
+++ benchmarks/ring_buffer.rs
@@ -1,11 +1,12 @@
 use std::hint::black_box;
 
 use criterion::{criterion_group, criterion_main, Criterion};
+use spsc::ring_buffer::{BufferReader, BufferWriter};
 
 const BUFFER_SIZE: usize = 4096;
 
 fn ring_buffer_1(v1: usize, v2: usize) {
-    let (writer, reader) = spsc::ring_buffer::RingBuffer::<u64>::new(BUFFER_SIZE).unwrap();
+    let (mut writer, mut reader) = spsc::ring_buffer::RingBuffer::<u64>::new(BUFFER_SIZE).unwrap();
 
     let reader = std::thread::spawn(move || {
         let mut read_buffer: Vec<u64> = (0..v1 as u64).map(|_| 0).collect();
@@ -13,7 +14,7 @@ fn ring_buffer_1(v1: usize, v2: usize) {
 
         while index != v1 {
             let n = reader.read(&mut read_buffer[index..index + v2]);
-            index += n;
+            index += n as usize;
         }
 
         read_buffer
@@ -25,7 +26,7 @@ fn ring_buffer_1(v1: usize, v2: usize) {
 
         while index != v1 {
             let n = writer.write(&write_buffer[index..index + v2]);
-            index += n;
+            index += n as usize;
         }
 
         write_buffer
